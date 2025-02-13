c66f1bccae24bca915c68298795df0bdb3c596ab
Tolulope Ojo <toluojo01@gmail.com>
2025-02-13 14:03:03 +0100

    revert books

--- api/routes/books.py
+++ api/routes/books.py
@@ -20,7 +20,7 @@ db.books = {
     ),
     2: Book(
         id=2,
-        title="The Lord of the Shit",
+        title="The Lord of the Rings",
         author="J.R.R. Tolkien",
         publication_year=1954,
         genre=Genre.FANTASY,
